ba5f923078d55e19e91f929617c949a3c2659022
oleg <legik7127@gmail.com>
2023-03-28 17:53:35 +0600

    added exceptions to input phone

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,6 +1,9 @@
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.WriteAbortedException;
 import java.util.InputMismatchException;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Scanner;
 
 /**
@@ -13,7 +16,7 @@ import java.util.Scanner;
  * дата_рождения - строка формата dd.mm.yyyy
  * номер_телефона - целое беззнаковое число без форматирования
  * пол - символ латиницей f или m.
- *
+ * <p>
  * Приложение должно проверить введенные данные по количеству. Если количество не совпадает с требуемым,
  * вернуть код ошибки, обработать его и показать пользователю сообщение, что он ввел меньше и больше данных,
  * чем требуется.
@@ -34,24 +37,61 @@ public class Program {
     public static void main(String[] args) throws IOException {
         inputString();
     }
+
     public static void inputString() throws IOException {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите ваши данные в виде: Фамилия Имя Отчество датарождения номертелефона пол");
 
-        String k = null;
+        String line = in.nextLine();
+        String[] dt = line.split(" ");
+        String sur = (dt[0]);
+        String name = (dt[1]);
+        String surN = (dt[2]);
+        String born = (dt[3]);
+        String tel = (dt[4]);
+        String gender = (dt[5]);
         try {
-            k = in.next();
-            System.out.println("Вам удалось ввести валидные данные: " + k);
-        } catch (InputMismatchException e) {
-            System.out.println("Введите корректное дробное число типа float");
+            if (tel.length() < 10 || tel.length() > 10)
+                throw new RuntimeException();
+//            if (sur.length() < 1 || name.length() < 1 || surN == null || born == null || tel == null || gender == null)
+//                throw new ArrayIndexOutOfBoundsException();
+//            if (gender.length() > 1)
+//                throw new InvalidPropertiesFormatException("введены неверные данные по полу");
+//            if (gender != "f" && gender != "m")
+//                throw new InvalidPropertiesFormatException("введены неверные данные по полу");
+        } catch (RuntimeException e) {
+            System.out.println("введите корректно номер телефона 10 цифр подряд, - например 9267004070");
             inputString();
         }
-        finally {
-            FileWriter writer = new FileWriter("data.csv");
-            writer.write(k);
-        }
 
+//        }catch (ArrayIndexOutOfBoundsException e){
+//                System.out.println("Введите 6 верных аргументов");
+//
+//        } catch (InvalidPropertiesFormatException e) {
+//            System.out.println("Введите верные данные по полу: символ латиницей f или m");
+//            inputString();
+//        }
+
+        System.out.println(sur + " " + name + " " + surN + " " + born + " " + tel + " " + gender +
+                " - данные успешно сохранены");
         in.close();
+
+//        String k = null;
+//        try {
+//            k = in.next();
+//            System.out.println("Вам удалось ввести валидные данные: " + k);
+//        } catch (InputMismatchException e) {
+//            System.out.println("Введите корректное дробное число типа float");
+//            inputString();
+//        }
+//        finally {
+//            FileWriter writer = new FileWriter("data.csv");
+//            writer.write(k);
+//            writer.close();
+//        }
+//
+//        in.close();
     }
 }
+
 
